15b4c5f403e2f783ccb79df91a1b45b14b94e3f5
Orlewilson B. Maia <prof.orlewilson@gmail.com>
2021-12-08 22:44:31 -0400

    Add Exemplo Hello World em NodeJS via console
    Signed-off-by: Orlewilson B. Maia <prof.orlewilson@gmail.com>

---
+++ 01-hello-world-nodejs-console/index.js
@@ -0,0 +1,11 @@
+/*
+  Oficina Como Criar Coisas Inteligentes com NodeJS e Arduino - 10/12/2021
+  Feira do Polo Digital de Manaus 2021
+  Facilitador:       Orlewilson Bentes Maia
+  Data Criacao:      08/12/2021
+  Data Atualizacao:  08/12/2021
+  Descricao:         Hello World NodeJS no console
+*/
+
+// mostra mensagem no console
+console.log("Hello World! Seja bem-vindo ao NodeJS!");
